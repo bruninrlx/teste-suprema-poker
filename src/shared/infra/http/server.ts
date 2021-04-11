33b7fa6236e41784afbbe52529c63b5d222597d6
Bruno Leandro <bruno.lendro2000@gmail.com>
2021-04-11 19:44:56 -0300

    feat(backend): salvando Procfile

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -10,6 +10,6 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333, () => {
-  console.log('server started on port 3333!');
+app.listen(5000, () => {
+  console.log('server started on port 5000!');
 });
